4bf36b4a1f251e38cd7412ee65e4e4231d781b10
Pranav Pusarla <ppusarla@gmail.com>
2020-07-23 10:50:31 -0700

    added augmented images to conv NN

--- models/convNN.py
+++ models/convNN.py
@@ -3,10 +3,18 @@ import tensorflow as tf
 from tensorflow.keras import models, layers
 import matplotlib.pyplot as  plt
 
-X = np.load('../preprocessing/ImageData.npy')
-y = np.load('../preprocessing/labels.npy')
-print(X.shape)
-print(y.shape)
+image_data = np.load('../preprocessing/ImageData.npy')
+labels = np.load('../preprocessing/labels.npy')
+flipped_image_data = np.load('../imageAugmentation/flipped_ImageData.npy')
+flipped_labels = np.load('../imageAugmentation/flipped_labels.npy')
+mirrored_image_data = np.load('../imageAugmentation/mirrored_ImageData.npy')
+mirrored_labels = np.load('../imageAugmentation/mirrored_labels.npy')
+
+X = np.concatenate((image_data, flipped_image_data, mirrored_image_data), axis=0)
+#print(X.shape)
+
+y = np.concatenate((labels, flipped_labels, mirrored_labels), axis=0)
+#print(y.shape)
 
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(128, 128, 3)))
@@ -19,7 +27,7 @@ model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(2))
 
 model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
-training_history = model.fit(X, y, epochs=30)
+training_history = model.fit(X, y, epochs=11)
 
 plt.plot(training_history.history['accuracy'], label='accuracy')
 plt.xlabel("Epoch")
